d19caca3e0ca88e7f58f752100613c961854aa09
Mohammed Fahad <m767@hotmail.com>
2017-11-16 12:42:21 +0300

    تم تنسيق الصفحة
    وتعديل المسافة بين الليبل والانبوت ,تم تعديل مكان البوتون

--- add.php
+++ add.php
@@ -8,8 +8,8 @@
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-rtl/3.4.0/css/bootstrap-rtl.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-<link rel="stylesheet" href="css/style.css">
-    <script src="js/script.js"></script>
+  <link rel="stylesheet" href="css/style.css">
+  <script src="js/script.js"></script>
     <script>
     $(document).ready(function(){
         
@@ -24,25 +24,28 @@
 
 <div class="container">
 
+  <p>
 <h2 align="center" style="background-color: #A7A7A7">صفحة اضافة</h2>
+  </p> 
    
    
    
-   
-    <p>  
-   <label class="custom-file">ادخل صورة &nbsp; &nbsp; &nbsp;
-   <input type="file" id="file" class="custom-file-input">
-  <span class="custom-file-control"></span>
-</label>
-      </p>
-    
-    
+ <div class="row">   
+   <div class="col-sm-2">
+       <label class="custom-file">ادخل صورة
+   </div>
+      <div class="col-sm-10">
+       <input type="file" id="file" class="custom-file-input">
+       <span class="custom-file-control"></span>
+       </label>  
+ </div>      
+ </div> 
+   <br> 
     
     
     
     
 <div class="row">   
-
  <div class="col-sm-2">
   <label for="country">نوع اللباس</label>
  </div>   
@@ -59,27 +62,46 @@
     </ul>
    </div>
   </div>
-  
-  
 </div>
+<br>
 
-  
 
-<div class="bd-example" data-example-id="">
-<form>
-  <div class="form-group">
-    <label for="fo">اسم المتجر&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;</label>
-    <input type="text" class="form-control-static" id="fo" placeholder="اسم المتجر">
+
+<form>  
+ <div class="row">   
+   <div class="bd-example" data-example-id="">
+     <div class="col-sm-2">
+    <div class="form-group">
+  <label for="fo">اسم المتجر</label>
+    </div>
+      </div>
+    
+    
+    
+    
+     <div class="col-sm-10">
+   <input type="text" class="form-control-static" id="fo" placeholder="اسم المتجر">
+     </div>
   </div>
+ </div>
 </form>
+<br>
+
+
+
+<div class="row">   
+   <div class="col-sm-5">
+   </div>
+   	    <div class="col-sm-7">
+           <button type="submit" class="btn btn-primary">اضافة</button>
+           <button type="result" class="btn btn-danger">الغاء</button>
+	    </div>
 </div>
-&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
-&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
-<button type="submit" class="btn btn-primary">حفظ</button>
-<button type="result" class="btn btn-danger">الغاء</button>
+
+
+
+
 <script>
-	
-	
 function myFunction() {
     var x = document.getElementById("myTopnav");
     if (x.className === "topnav") {
